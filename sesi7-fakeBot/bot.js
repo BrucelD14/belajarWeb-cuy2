e933bf6b0c9e8cae972b7949d89e04cee9daef70
BrucelD14 <samuderaduta144@gmail.com>
2022-11-20 12:55:04 +0700

    add starting bot

--- sesi7-fakeBot/bot.js
+++ sesi7-fakeBot/bot.js
@@ -1,1 +1,14 @@
-console.log('testing');
+const pertanyaan = document.getElementById("pertanyaan");
+const jawaban = document.getElementById("jawaban");
+
+let init = 0;
+
+const botSay = () => {
+  return [`Halo, perkenalkan nama saya brucebot. Siapa nama kamu??`];
+};
+
+pertanyaan.innerHTML = botSay()[0];
+
+function botStart() {
+  console.log("starting bot");
+}
