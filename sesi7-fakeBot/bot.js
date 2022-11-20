a7c624c3761133c21ad6ce60950c99f00f36a722
BrucelD14 <samuderaduta144@gmail.com>
2022-11-20 22:31:36 +0700

    add interaction bot

--- sesi7-fakeBot/bot.js
+++ sesi7-fakeBot/bot.js
@@ -3,22 +3,30 @@ const jawaban = document.getElementById("jawaban");
 
 let init = 0;
 
-const botSay = () => {
-  return [`Halo, perkenalkan nama saya brucebot. Siapa nama kamu??`];
+const botSay = (data) => {
+  return [
+    `Halo, perkenalkan nama saya brucebot. Siapa nama kamu??`,
+    `Halo ${data?.nama}, berapa usia kamu?`,
+    `Ohh ${data?.usia}, hobi kamu apa yaa?`,
+    `wahh sama dong aku juga hobi ${data?.hobi}, btw punya pacar nggak?`,
+    `Oooh ${data?.pacar}, ya udah kalau gitu. Udahan yaa`,
+  ];
 };
 
 pertanyaan.innerHTML = botSay()[0];
 
+let userData = [];
+
 function botStart() {
   init++;
   if (init === 1) {
-    console.log({ nama: jawaban.value });
+    botdelay({ nama: jawaban.value });
   } else if (init === 2) {
-    console.log({ usia: jawaban.value });
+    botdelay({ usia: jawaban.value });
   } else if (init === 3) {
-    console.log({ hobi: jawaban.value });
+    botdelay({ hobi: jawaban.value });
   } else if (init === 4) {
-    console.log({ pacar: jawaban.value });
+    botdelay({ pacar: jawaban.value });
   } else if (init === 5) {
     finishing();
   } else {
@@ -26,10 +34,20 @@ function botStart() {
   }
 }
 
+function botdelay(jawabanUser) {
+  console.log({ userData: userData });
+  setTimeout(() => {
+    pertanyaan.innerHTML = botSay(jawabanUser)[init];
+  }, [800]);
+  userData.push(jawaban.value);
+  jawaban.value = "";
+}
+
 function finishing() {
-  console.log("finishing...");
+  pertanyaan.innerHTML = `Makasih yaa ${userData[0]} dah main ke brucebot :), kali kali kita main ${userData[2]} bareng yuk`;
+  jawaban.value = "sama-sama brucebot";
 }
 
 function botEnd() {
-  console.log("bot end...");
+  window.location.reload();
 }
